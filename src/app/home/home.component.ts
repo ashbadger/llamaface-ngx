2d65dadad62cc49ab9f709207cd138dd76adcf80
ASHLY BADGETT <ashlybadgett@gmail.com>
2017-11-27 20:21:20 -0600

    fix(home): added back view, fix compiling issue

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,6 @@ import { Router } from '@angular/router'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
   providers: [LoginService]
 })
 export class HomeComponent implements OnInit {
